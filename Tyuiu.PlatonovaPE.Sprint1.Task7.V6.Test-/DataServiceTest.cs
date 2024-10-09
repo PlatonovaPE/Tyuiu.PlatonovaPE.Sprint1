625849ec9e47702852cb6a5158c4283436d39c49
Tyuiu.PlatonovaPE@outlook.com <Tyuiu.PlatonovaPE@outlook.com>
2024-10-09 15:35:13 +0500

    Task7

---
+++ Tyuiu.PlatonovaPE.Sprint1.Task7.V6.Test-/DataServiceTest.cs
@@ -0,0 +1,18 @@
+ using Tyuiu.PlatonovaPE.Sprint1.Task7.V6.Lib;
+    namespace Tyuiu.PlatonovaPE.Sprint1.Task7.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 2;
+            double wait = -22;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PlatonovaPE.Sprint1.Task7.V6/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.PlatonovaPE.Sprint1.Task7.V6.Lib;
+namespace Tyuiu.PlatonovaPE.Sprint1.Task7.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Исходные данные                                                              *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("( 1 + 1/ X** 2) ** x - 12 * x ** 2 * y");
+
+
+            double x, y;
+
+            Console.WriteLine("Введите значение Х:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение У:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Результат:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+
+            Console.ReadKey();
+
+        }
+    }
+}
